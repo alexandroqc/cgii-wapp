6d99460f1245c9dc5bb43e0dcb420303a511f1bc
Alejandro Quisbert <alexandro@autistici.org>
2019-04-01 18:29:47 -0400

    Cleaning unnecesary APIs

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,7 @@
-import examplesRouter from './api/controllers/examples/router';
+// import examplesRouter from './api/controllers/examples/router';
 import webSitesRouter from './api/controllers/websites/router';
 
 export default function routes(app) {
-  app.use('/api/v1/examples', examplesRouter);
+  // app.use('/api/v1/examples', examplesRouter);
   app.use('/api/v1/wapp', webSitesRouter);
 }
