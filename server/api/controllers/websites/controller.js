52bc4769758782763b74eac5f50ecdee31ce9215
Alejandro Quisbert <alexandro@autistici.org>
2019-04-01 18:24:58 -0400

    Response code

--- server/api/controllers/websites/controller.js
+++ server/api/controllers/websites/controller.js
@@ -7,15 +7,17 @@ const Browser = require('wappalyzer/browsers/zombie');
 export class Controller {
   wapp(req, res) {
     const errorMessage = {
-      error: "Ooops, something goes wrong"
+      error: "Ooops!, your domain does not exist"
     }
     const options = {};
     const wappalyzer = new Wappalyzer(Browser, req.params.uri, options)
     wappalyzer.analyze()
       .then((json) => {
+        res.status(200);
         return res.send(JSON.stringify(json));
       })
       .catch((error) => {
+        res.status(404);
         errorMessage.error = error;
         return res.send(JSON.stringify(errorMessage));
       });
